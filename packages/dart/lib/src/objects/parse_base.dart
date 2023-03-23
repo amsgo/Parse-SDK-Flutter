9b908c35dee5a4901f00b99ee85eac2116c51730
Jonas Heitmann <jonas@chronos-arts.de>
2023-03-23 07:49:27 +0100

    Make getObjectData() public

--- packages/dart/lib/src/objects/parse_base.dart
+++ packages/dart/lib/src/objects/parse_base.dart
@@ -167,7 +167,7 @@ abstract class ParseBase {
 
   /// Returns the objects variables
   @protected
-  Map<String, dynamic> _getObjectData() => _objectData;
+  Map<String, dynamic> getObjectData() => _objectData;
 
   bool containsValue(Object value) {
     return _getObjectData().containsValue(value);
